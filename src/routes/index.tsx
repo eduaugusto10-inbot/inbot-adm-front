b05de53d82689745cb9ddf73d7d7daee44cd0c57
Tom-Inbot <tomas.martinez@inbot.com.br>
2024-02-28 13:32:14 -0300

    Pagina OAuth adicionada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,16 @@ import AddNumber from "../Smarters/AddNumber"
 import AllPhones from "../Smarters"
 import ChangeDeleteNumber from "../Smarters/ChangeDeleteNumber"
 import Panel from "../pages/RelatorioGestao/Panel"
+import Oauth from "../pages/OAuth/Oauth"
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
             <Route path='/add' element={<AddNumber />} />
             <Route path='/list' element={<Panel />} />
+            <Route path='/oauth' element={<Oauth />} />
             <Route path='/' element={<AllPhones />} />
-            {<Route path='/change' element={<ChangeDeleteNumber />} />}
+            <Route path='/change' element={<ChangeDeleteNumber />} />
         </Routes>
     </BrowserRouter>
 
