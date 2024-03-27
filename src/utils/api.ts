1ba4d4fafb3e7b7087bc4b59dc342e8ab8eb9fc4
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2024-03-27 01:27:19 -0300

    Alterado os endpoints para o cluster novo

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:19000'
-    // baseURL: 'https://webhooks.inbot.com.br/inbot-adm-back/v1/gateway'
+    // baseURL: 'http://localhost:19000'
+    baseURL: 'https://webhooks.inbot.com.br/inbot-adm-back/v1/gateway'
 })
 
 export default api
