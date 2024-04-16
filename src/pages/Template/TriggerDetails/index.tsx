cdb1ef53fda1b86a6b9cc3b082b8980ff992a39d
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2024-04-15 23:56:16 -0300

    Adicionado filtro na tela de detalhes do disparo

--- src/pages/Template/TriggerDetails/index.tsx
+++ src/pages/Template/TriggerDetails/index.tsx
@@ -4,33 +4,226 @@ import { useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { adjustTime, mask } from "../../../utils/utils";
 import { ICustomer } from "../../types";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+interface Filters {
+    telefone: string;
+    variable_1: string;
+    variable_2: string;
+    variable_3: string;
+    variable_4: string;
+    variable_5: string;
+    variable_6: string;
+    variable_7: string;
+    variable_8: string;
+    variable_9: string;
+    status: {
+        aguardando: boolean;
+        enviado: boolean;
+        erro: boolean;
+    };
+    // Adicione outros filtros aqui, se necessário
+}
 
 export function TriggerDetails() {
+    ChartJS.register(ArcElement, Tooltip, Legend);
 
     const location = useLocation()
     const triggerId = location.state.triggerId;
     const profilePic = location.state.urlLogo;
     const [customerStatus, setCustomerStatus] = useState<ICustomer[]>([])
+    const [waiting, setWaiting] = useState<number>(0)
+    const [send, setSend] = useState<number>(0)
+    const [erro, setErro] = useState<number>(0)
+    const [filters, setFilters] = useState<Filters>({
+        telefone: '',
+        variable_1: '',
+        variable_2: '',
+        variable_3: '',
+        variable_4: '',
+        variable_5: '',
+        variable_6: '',
+        variable_7: '',
+        variable_8: '',
+        variable_9: '',
+        status: {
+            aguardando: true,
+            enviado: true,
+            erro: true,
+        }
+    });
+
+    const dataPie = {
+        labels: ["Aguardando", "Enviado", "Erro"],
+        datasets: [
+            {
+                label: 'Quantidade',
+                data: [waiting, send, erro],
+                backgroundColor: [
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
 
     useEffect(() => {
         api.get(`/whats-customer/${triggerId}`)
-            .then(resp => setCustomerStatus(resp.data.data))
+            .then(resp => {
+                setCustomerStatus(resp.data.data)
+                let aguardando = 0;
+                let erro = 0;
+                let enviado = 0;
+                for (let i = 0; i < resp.data.data.length; i++) {
+                    if (resp.data.data[i].status === "Enviado") {
+                        enviado++;
+                    } else if (resp.data.data[i].status === "Erro") {
+                        erro++;
+                    } else {
+                        aguardando++;
+                    }
+                }
+                setWaiting(aguardando);
+                setErro(erro);
+                setSend(enviado);
+            })
             .catch(error => console.log(error))
-    },[])
-
+    }, [])
+    const handleTelefoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, telefone: (event.target.value) });
+    };
+    const handleVariable1Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_1: event.target.value });
+    };
+    const handleVariable2Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_2: event.target.value });
+    };
+    const handleVariable3Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_3: event.target.value });
+    };
+    const handleVariable4Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_4: event.target.value });
+    };
+    const handleVariable5Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_5: event.target.value });
+    };
+    const handleVariable6Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_6: event.target.value });
+    };
+    const handleVariable7Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_7: event.target.value });
+    };
+    const handleVariable8Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_8: event.target.value });
+    };
+    const handleVariable9Change = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilters({ ...filters, variable_9: event.target.value });
+    };
+    const handleStatusChange = (statusKey: keyof Filters['status']) => {
+        setFilters({
+            ...filters,
+            status: {
+                ...filters.status,
+                [statusKey]: !filters.status[statusKey],
+            },
+        });
+    };
+    const filteredCustomers = customerStatus.filter(customer => {
+        if (filters.telefone !== '' && !(customer.phone).includes(filters.telefone)) {
+            return false;
+        }
+        if (customer.variable_1 !== null && filters.variable_1 !== '' && !customer.variable_1.toLowerCase().includes(filters.variable_1.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_2 !== null && filters.variable_2 !== '' && !customer.variable_2.toLowerCase().includes(filters.variable_2.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_3 !== null && filters.variable_3 !== '' && !customer.variable_3.toLowerCase().includes(filters.variable_3.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_4 !== null && filters.variable_4 !== '' && !customer.variable_4.toLowerCase().includes(filters.variable_4.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_5 !== null && filters.variable_5 !== '' && !customer.variable_5.toLowerCase().includes(filters.variable_5.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_6 !== null && filters.variable_6 !== '' && !customer.variable_6.toLowerCase().includes(filters.variable_6.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_7 !== null && filters.variable_7 !== '' && !customer.variable_7.toLowerCase().includes(filters.variable_7.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_8 !== null && filters.variable_8 !== '' && !customer.variable_8.toLowerCase().includes(filters.variable_8.toLowerCase())) {
+            return false;
+        }
+        if (customer.variable_9 !== null && filters.variable_9 !== '' && !customer.variable_9.toLowerCase().includes(filters.variable_9.toLowerCase())) {
+            return false;
+        }
+        if (
+            (filters.status.aguardando && customer.status === 'aguardando') ||
+            (filters.status.enviado && customer.status === 'Enviado') ||
+            (filters.status.erro && customer.status === 'Erro')
+        ) {
+            return true;
+        }
+        return false;
+    });
     return (
         <div>
             <ToastContainer />
             <div>
-                <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+                <div style={{ display: "flex", flexDirection: "row", width: "100%", padding: "20px 0px 0px 20px" }}>
                     <img src={profilePic} width={100} height={100} alt='logo da empresa' style={{ marginBottom: "-30px" }} />
                 </div>
                 <div style={{ width: "100%", borderBottom: "1px solid #000", marginBottom: "30px", display: "flex", flexDirection: "row" }}>
                     <h1 style={{ fontSize: "23px", fontWeight: "bolder", color: "#324d69", width: "90%" }}>Log de Disparos</h1>
                 </div>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                    <div style={{ width: "250px", height: "300px", border: "1px solid #010043", margin: "20px" }}>
+                        <span style={{ fontWeight: "bolder" }}>Resumo do Disparo</span>
+                        <Pie data={dataPie} />
+                    </div>
+                    <div style={{fontWeight:"bolder", border:"1px solid #000", height:"250px", margin: "20px"}}>
+                        <span>Filtros</span>
+                        <div style={{ display: "flex", flexDirection: "row" }}>
+                            <div style={{ display: "flex", flexDirection: "column", margin: "10px" }}>
+                                <span>Status</span>
+                                <div style={{ display: "flex", flexDirection: "column", margin: "10px", textAlign:"left" }}>
+                                    <div><input type="checkbox" onChange={() => handleStatusChange('aguardando')} checked={filters.status.aguardando} /><span style={{marginLeft:"5px", fontWeight:"normal"}}>Aguardando</span></div>
+                                    <div><input type="checkbox" onChange={() => handleStatusChange('enviado')} checked={filters.status.enviado} /><span style={{marginLeft:"5px", fontWeight:"normal"}}>Enviado</span></div>
+                                    <div><input type="checkbox" onChange={() => handleStatusChange('erro')} checked={filters.status.erro} /><span style={{marginLeft:"5px", fontWeight:"normal"}}>Erro</span></div>
+                                </div>
+                            </div>
+                            <div style={{ display: "flex", flexDirection: "column", margin: "10px" }}>
+                                <span>Telefone</span><input type="text" value={filters.telefone} onChange={handleTelefoneChange} placeholder="Digite o telefone..." />
+                                <span>Variável 1</span><input type="text" value={filters.variable_1} onChange={handleVariable1Change} placeholder="Digite valor..." />
+                                <span>Variável 2</span><input type="text" value={filters.variable_2} onChange={handleVariable2Change} placeholder="Digite valor..." />
+                            </div>
+                            <div style={{ display: "flex", flexDirection: "column", margin: "10px" }}>
+                                <span>Variável 3</span><input type="text" value={filters.variable_3} onChange={handleVariable3Change} placeholder="Digite valor..." />
+                                <span>Variável 4</span><input type="text" value={filters.variable_4} onChange={handleVariable4Change} placeholder="Digite valor..." />
+                                <span>Variável 5</span><input type="text" value={filters.variable_5} onChange={handleVariable5Change} placeholder="Digite valor..." />
+                            </div>
+                            <div style={{ display: "flex", flexDirection: "column", margin: "10px" }}>
+                                <span>Variável 6</span><input type="text" value={filters.variable_6} onChange={handleVariable6Change} placeholder="Digite valor..." />
+                                <span>Variável 7</span><input type="text" value={filters.variable_7} onChange={handleVariable7Change} placeholder="Digite valor..." />
+                                <span>Variável 8</span><input type="text" value={filters.variable_8} onChange={handleVariable8Change} placeholder="Digite valor..." />
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <table>
                     <thead>
-                        <tr className="cells" style={{ backgroundColor: "#010043" }}>
+                        <tr className="cells" style={{ backgroundColor: "#010043", fontSize: "12px" }}>
                             <th className="cells">Telefone</th>
                             <th className="cells">Var. 1</th>
                             <th className="cells">Var. 2</th>
@@ -47,23 +240,23 @@ export function TriggerDetails() {
                             <th className="cells">Log</th>
                         </tr>
                     </thead>
-                    {customerStatus.map(customer=>(
-                    <tr>
-                        <td><span>{mask(customer.phone)}</span></td>
-                        <td><span>{customer.variable_1}</span></td>
-                        <td><span>{customer.variable_2}</span></td>
-                        <td><span>{customer.variable_3}</span></td>
-                        <td><span>{customer.variable_4}</span></td>
-                        <td><span>{customer.variable_5}</span></td>
-                        <td><span>{customer.variable_6}</span></td>
-                        <td><span>{customer.variable_7}</span></td>
-                        <td><span>{customer.variable_8}</span></td>
-                        <td><span>{customer.variable_9}</span></td>
-                        <td><span>{customer.status}</span></td>
-                        <td><span>{adjustTime(customer.data_criacao)}</span></td>
-                        <td><span>{customer.data_disparo ? adjustTime(customer.data_disparo): "----"}</span></td>
-                        <td><span> -- </span></td>
-                    </tr>
+                    {filteredCustomers.map(customer => (
+                        <tr>
+                            <td><span style={{ fontSize: "12px" }}>{mask(customer.phone)}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_1}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_2}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_3}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_4}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_5}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_6}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_7}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_8}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.variable_9}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.status}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{adjustTime(customer.data_criacao)}</span></td>
+                            <td><span style={{ fontSize: "12px" }}>{customer.data_disparo ? adjustTime(customer.data_disparo) : "----"}</span></td>
+                            <td><span style={{ fontSize: "12px" }}> -- </span></td>
+                        </tr>
                     ))}
                 </table>
             </div>
