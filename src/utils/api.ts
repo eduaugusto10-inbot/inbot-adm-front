cac2390d87cbc0954a7fe603fe8e0c482430c587
Eduardo Augusto Gomes de Oliveira <eduardo.oliveira@inbot.com.br>
2025-08-26 11:30:09 -0300

    atualizacao das variaveis de ambiente

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,20 +1,7 @@
 import axios from "axios";
 
-// const generateToken = async (botId: number): Promise<string | null> => {
-//   try {
-//     const resp = await api.get(`/customer-manager/access-key/${botId}`);
-//     return resp.data.key;
-//   } catch (error) {
-//     console.error("Erro ao buscar access key:", error);
-//     return null;
-//   }
-// };
-
 const api = axios.create({
-  // baseURL: 'http://localhost:19000',
-  // baseURL: "https://api-stg.inbot.com.br/user-manager/v1",
   baseURL: process.env.REACT_APP_BASE_URL,
-  // baseURL: "https://api.inbot.com.br/user-manager/v1",
 });
 
 export default api;
